8bcb082a2a50ef944aec70402ff662d5ce00ae99
David Haynes <david.e.haynes@stfc.ac.uk>
2014-01-22 16:56:03 +0000

    Added test string to develop login

--- debug/vmware-apis.sh
+++ debug/vmware-apis.sh
@@ -52,6 +52,38 @@ https://cemscloud.jc.rl.ac.uk:443/api/versions`
     | sort | uniq | wc -l
 }
 
+read -r -d '' LOGINTEST <<'END'
+HTTP/1.1 200 OK
+Date: Tue, 14 Jan 2014 10:58:48 GMT
+x-vcloud-authorization: TF6QoPX2xZbwVZM0yT4QmMfM6nKvC2Yz/HqkuFdpU0U=
+Set-Cookie: vcloud-token=TF6QoPX2xZbwVZM0yT4QmMfM6nKvC2Yz/HqkuFdpU0U=; Secure;
+Path=/
+Content-Type: application/vnd.vmware.vcloud.session+xml;version=1.5
+Date: Tue, 14 Jan 2014 10:58:48 GMT
+Content-Length: 884
+
+<?xml version="1.0" encoding="UTF-8"?>
+<Session xmlns="http://www.vmware.com/vcloud/v1.5" user="dehaynes"
+org="CEMSTest" type="application/vnd.vmware.vcloud.session+xml"
+href="https://vcloud.cems.rl.ac.uk/api/session/"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xsi:schemaLocation="http://www.vmware.com/vcloud/v1.5
+http://vcloud.cems.rl.ac.uk/api/v1.5/schema/master.xsd">
+  <Link rel="down" type="application/vnd.vmware.vcloud.orgList+xml"
+href="https://vcloud.cems.rl.ac.uk/api/org/"/>
+  <Link rel="down" type="application/vnd.vmware.admin.vcloud+xml"
+href="https://vcloud.cems.rl.ac.uk/api/admin/"/>
+  <Link rel="down" type="application/vnd.vmware.vcloud.query.queryList+xml"
+href="https://vcloud.cems.rl.ac.uk/api/query"/>
+  <Link rel="entityResolver" type="application/vnd.vmware.vcloud.entity+xml"
+href="https://vcloud.cems.rl.ac.uk/api/entity/"/>
+</Session>
+END
+
+api_login()
+{
+    echo "$LOGINTEST"
+}
 
 while test $# -gt 0
 do
@@ -77,6 +109,7 @@ do
     shift
 done
 
+
 if [ `api_versions` = "0" ]
 then
     warning "API is not responding"
@@ -84,5 +117,5 @@ then
 else
     echo "API is responding"
 fi
-
+api_login
 warning "That's your lot, $USER!"
