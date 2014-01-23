66c96c8003e0a39a632a0e2b97b790bff41e6651
David Haynes <david.e.haynes@stfc.ac.uk>
2014-01-23 17:02:30 +0000

    vmware-apis.sh --settings works with phase01 and phase02

--- debug/vmware-apis.sh
+++ debug/vmware-apis.sh
@@ -13,7 +13,6 @@ PROGRAM=$( basename "$0" )
 DIR=$( cd "$( dirname "$0" )" && pwd )
 PARENT=$(readlink -e "$DIR/..")
 SETTINGS="phase01"
-CFG="$PARENT/../cloudhands-jasmin/cloudhands/jasmin/vcloud/$SETTINGS.cfg"
 
 usage()
 {
@@ -51,12 +50,20 @@ version()
 
 # End of boilerplate
 
-api_versions()
+read_endpoint()
 {
-    REPLY=`curl -s -k -H 'Accept:application/*+xml;version=1.5' -X GET \
-https://cemscloud.jc.rl.ac.uk:443/api/versions`
-    echo $REPLY | tr ' ' '\n' | grep -E -o "v[[:digit:]]*\.[[:digit:]]*" \
-    | sort | uniq | wc -l
+    # Read the variables 'name', 'port' and 'api_version' from the user section
+    # of a config file
+    if [ -f "$CFG" ]
+    then
+        while read -r key value; do
+            eval "host_$key"="\$value"
+        done < <(grep -A3 '\[host\]' $CFG \
+                | tr -d ' ' | cut -s -d'=' -f1,2 --output-delimiter=" ")
+    else
+        warning "Could not find settings ($CFG)"
+        exit 0
+    fi
 }
 
 read_credentials()
@@ -66,7 +73,7 @@ read_credentials()
     if [ -f "$CFG" ]
     then
         while read -r key value; do
-            eval user_$key=\$value
+            eval "user_$key"="\$value"
         done < <(grep -A2 '\[user\]' $CFG \
                 | tr -d ' ' | cut -s -d'=' -f1,2 --output-delimiter=" ")
     else
@@ -75,19 +82,28 @@ read_credentials()
     fi
 }
 
-api_login()
+api_versions()
+{
+    REPLY=`curl -s -k \
+    -H "Accept:application/*+xml;version=1.5" -X GET \
+    https://"$host_name":"$host_port"/api/versions`
+    echo "$REPLY" | tr ' ' '\n' | grep -E -o "v[[:digit:]]*\.[[:digit:]]*" \
+    | sort | uniq | wc -l
+}
+
+api_login() # user name, password
 {
     curl -s -i -k -u "$1:$2" \
-    -H 'Accept:application/*+xml;version=1.5' -X POST  \
-    https://cemscloud.jc.rl.ac.uk:443/api/sessions \
+    -H "Accept:application/*+xml;version="$host_api_version"" -X POST  \
+    https://"$host_name":"$host_port"/api/sessions \
     | grep 'x-vcloud-authorization.*'
 }
 
-api_org_get()
+api_org_get() # authorization token header
 {
-    curl -s -i -k -H 'Accept:application/*+xml;version=1.5' \
-    -H "$1" \
-    -X GET https://cemscloud.jc.rl.ac.uk:443/api/org
+    curl -s -i -k -H "Accept:application/*+xml;version="$host_api_version"" \
+    -H "$1" -X GET \
+    https://"$host_name":"$host_port"/api/org
 }
 
 while test $# -gt 0
@@ -114,16 +130,27 @@ do
     shift
 done
 
+CFG="$PARENT/../cloudhands-jasmin/cloudhands/jasmin/vcloud/$SETTINGS.cfg"
+read_endpoint
+
+if [ ! -n "$host_name" -a "$host_port" -a "$host_api_version" ]
+then
+    warning "Could not configure settings."
+    exit $EXITCODE
+else
+    info "Endpoint settings configured."
+fi
 
 if [ `api_versions` = "0" ]
 then
     warning "API is not responding"
     exit $EXITCODE
 else
-    info "Endpoint contacted successfully."
+    info "$host_name contacted successfully."
 fi
 
 read_credentials
+
 TOKEN=`api_login "$user_name" "$user_pass"`
 echo `api_org_get "$TOKEN"`
 exit $EXITCODE
