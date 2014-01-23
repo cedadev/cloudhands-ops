36619735437629665034cdcef3678dc0813f67bb
David Haynes <david.e.haynes@stfc.ac.uk>
2014-01-23 16:35:01 +0000

    Fixed parameter quoting bug

--- debug/vmware-apis.sh
+++ debug/vmware-apis.sh
@@ -12,11 +12,12 @@ EXITCODE=0
 PROGRAM=$( basename "$0" )
 DIR=$( cd "$( dirname "$0" )" && pwd )
 PARENT=$(readlink -e "$DIR/..")
-CFG="$PARENT/../cloudhands-jasmin/cloudhands/jasmin/vcloud/phase01.cfg"
+SETTINGS="phase01"
+CFG="$PARENT/../cloudhands-jasmin/cloudhands/jasmin/vcloud/$SETTINGS.cfg"
 
 usage()
 {
-    echo "Usage: $PROGRAM [--user USER]"
+    echo "Usage: $PROGRAM [--settings SETTINGS]"
 }
 
 usage_and_exit()
@@ -25,27 +26,27 @@ usage_and_exit()
     exit "$1"
 }
 
-version()
-{
-    VERSION=$(head "$PARENT/cloudhands/ops/__init__.py" | cut -d= -f2 | tr -d '" ')
-    echo "$PROGRAM $VERSION"
-}
-
 info()
 {
-    echo "$@" 1>&2
+    echo -e "\e[2m$@\e[0m" 1>&2
 }
 
 warning()
 {
-    info "$@"
+    echo -e "\e[1m$@\e[0m" 1>&2
     EXITCODE=$(($EXITCODE + 1))
 }
 
 error()
 {
-    info "$@"
+    warning "$@"
     usage_and_exit 1
+}
+
+version()
+{
+    VERSION=$(head "$PARENT/cloudhands/ops/__init__.py" | cut -d= -f2 | tr -d '" ')
+    echo "$PROGRAM $VERSION"
 }
 
 # End of boilerplate
@@ -74,34 +75,6 @@ read_credentials()
     fi
 }
 
-read -r -d '' TEST_LOGIN <<'END'
-HTTP/1.1 200 OK
-Date: Tue, 14 Jan 2014 10:58:48 GMT
-x-vcloud-authorization: TF6QoPX2xZbwVZM0yT4QmMfM6nKvC2Yz/HqkuFdpU0U=
-Set-Cookie: vcloud-token=TF6QoPX2xZbwVZM0yT4QmMfM6nKvC2Yz/HqkuFdpU0U=; Secure;
-Path=/
-Content-Type: application/vnd.vmware.vcloud.session+xml;version=1.5
-Date: Tue, 14 Jan 2014 10:58:48 GMT
-Content-Length: 884
-
-<?xml version="1.0" encoding="UTF-8"?>
-<Session xmlns="http://www.vmware.com/vcloud/v1.5" user="dehaynes"
-org="CEMSTest" type="application/vnd.vmware.vcloud.session+xml"
-href="https://vcloud.cems.rl.ac.uk/api/session/"
-xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-xsi:schemaLocation="http://www.vmware.com/vcloud/v1.5
-http://vcloud.cems.rl.ac.uk/api/v1.5/schema/master.xsd">
-  <Link rel="down" type="application/vnd.vmware.vcloud.orgList+xml"
-href="https://vcloud.cems.rl.ac.uk/api/org/"/>
-  <Link rel="down" type="application/vnd.vmware.admin.vcloud+xml"
-href="https://vcloud.cems.rl.ac.uk/api/admin/"/>
-  <Link rel="down" type="application/vnd.vmware.vcloud.query.queryList+xml"
-href="https://vcloud.cems.rl.ac.uk/api/query"/>
-  <Link rel="entityResolver" type="application/vnd.vmware.vcloud.entity+xml"
-href="https://vcloud.cems.rl.ac.uk/api/entity/"/>
-</Session>
-END
-
 api_login()
 {
     curl -s -i -k -u "$1:$2" \
@@ -110,11 +83,18 @@ api_login()
     | grep 'x-vcloud-authorization.*'
 }
 
+api_org_get()
+{
+    curl -s -i -k -H 'Accept:application/*+xml;version=1.5' \
+    -H "$1" \
+    -X GET https://cemscloud.jc.rl.ac.uk:443/api/org
+}
+
 while test $# -gt 0
 do
     case $1 in
-        --user )
-            USER=$2
+        --settings )
+            SETTINGS=$2
             shift
             ;;
         --help | -h )
@@ -144,8 +124,6 @@ else
 fi
 
 read_credentials
-TOKEN=`api_login $user_name $user_pass`
-
-curl -i -k -H 'Accept:application/*+xml;version=1.5' \
--H "$TOKEN" \
--X GET https://cemscloud.jc.rl.ac.uk:443/api/org
+TOKEN=`api_login "$user_name" "$user_pass"`
+echo `api_org_get "$TOKEN"`
+exit $EXITCODE
