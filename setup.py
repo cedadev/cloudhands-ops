797e6c337883f86013f2cbb4c85b227a144337be
David Haynes <david.e.haynes@stfc.ac.uk>
2014-09-08 17:04:00 +0100

    Added cloudhands-common as a declared dependency. Upversion.

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=[
         "sphinxcontrib-seqdiag>=0.7.2",
         "execnet>=1.2.0",
+        "cloudhands-common>=0.29",
     ],
     entry_points={
         "console_scripts": [
