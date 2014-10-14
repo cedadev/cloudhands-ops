ce2dac1b081f88f44af35c744d612feb8ce11399
David Haynes <david.e.haynes@stfc.ac.uk>
2014-10-14 17:06:48 +0100

    Incorporating CatalogueChoice view into appliance JSON.

--- cloudhands/ops/test/test_puppet.py
+++ cloudhands/ops/test/test_puppet.py
@@ -10,8 +10,8 @@ APPLIANCE_API_RESPONSE = """
 {
     "info": {
         "page": {
-            "url": "http://jasmin-cloud.jc.rl.ac.uk/appliance/b1d0fe9485074c079d2fc524275a949d", 
-            "title": "Appliance parameters"
+            "title": "Appliance parameters", 
+            "url": "http://jasmin-cloud.jc.rl.ac.uk/appliance/b1d0fe9485074c079d2fc524275a949d"
         }, 
         "versions": {
             "cloudhands.jasmin": "0.50"
@@ -19,6 +19,7 @@ APPLIANCE_API_RESPONSE = """
     }, 
     "nav": {
         "nav_01": {
+            "name": "EOSCloud", 
             "_links": [
                 [
                     "EOSCloud", 
@@ -29,15 +30,12 @@ APPLIANCE_API_RESPONSE = """
                     [], 
                     null
                 ]
-            ], 
-            "name": "EOSCloud"
+            ]
         }
     }, 
     "items": {
         "item_01": {
-            "account": "/root", 
-            "_type": "publickey", 
-            "public_key": "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAzDpup+XwRKfAq5PtDYrseifyOFqWeAra3rONBzfdKub0Aa2imNjNFk+Q1Eeoqfn92A9bTx024EzoCg7daIswbi+ynXtzda+DT1RnpKcuOyOt3Jy8413ZOd+Ks3AovBzCQPpALiNwPu5zieCvBrd9lD4BNZo4tG6ELIv9Qv+APXPheGdDIMzwkhOf/8och4YkFGcVeYhTCjOdO3sFF8WkFmdW/OJP87RH9FBHLWMirdTz4x2tT+Cyfe47NUYCmxRkdulexy71OSIZopZONYvwx3jmradjt2Hq4JubO6wbaiUbF+bvyMJapRIPE7+f37tTSDs8W19djRf7DEz7MANprbw== cl@eduserv.org.uk", 
+            "uuid": "c9762dce02fd4af78e24f9df28845519", 
             "_links": [
                 [
                     "Public key", 
@@ -49,12 +47,12 @@ APPLIANCE_API_RESPONSE = """
                     null
                 ]
             ], 
-            "uuid": "478aaa9c523c4e33b60ff3d9e5fd3a9d"
+            "_type": "publickey", 
+            "public_key": "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAzDpup+XwRKfAq5PtDYrseifyOFqWeAra3rONBzfdKub0Aa2imNjNFk+Q1Eeoqfn92A9bTx024EzoCg7daIswbi+ynXtzda+DT1RnpKcuOyOt3Jy8413ZOd+Ks3AovBzCQPpALiNwPu5zieCvBrd9lD4BNZo4tG6ELIv9Qv+APXPheGdDIMzwkhOf/8och4YkFGcVeYhTCjOdO3sFF8WkFmdW/OJP87RH9FBHLWMirdTz4x2tT+Cyfe47NUYCmxRkdulexy71OSIZopZONYvwx3jmradjt2Hq4JubO6wbaiUbF+bvyMJapRIPE7+f37tTSDs8W19djRf7DEz7MANprbw== cl@eduserv.org.uk", 
+            "account": "/root"
         }, 
         "item_02": {
-            "account": "/users/bcumberbat", 
-            "_type": "publickey", 
-            "public_key": "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAuOg/gIR9szQ0IcPjqD1jlY9enJETyppW39MAH0WV1LqR+/ULulG4uBUS/HBwvS7ggu3P6mj4i2hH9Kz9JGwnkuhxMJu3d/b/2Z7/1hBkQls5BKTzSoYnPCVYfvPyNXzRHEcRPjyfGcrIYz2CU4g5Ei2f0IgRngamDQrTU33QLosoaJqfw0pvX2SdFyFRmJkY6vH7j66ciXl2bfUUdf1KaoadkD+n59U6EiURrholSlaZp0gECjx0dM4mZUD0DqjWGll0NmnM4NIpCl+lTOrFLicJBgPuAnsrqp8HjGEHweRoPwFkKpcPkfyV+k0o/bltu3Lyd8KLJrVzYAUXRnLRpw== dehaynes@snow.badc.rl.ac.uk", 
+            "uuid": "37323dfca0ab4b64b01aea1ee2a399b9", 
             "_links": [
                 [
                     "Public key", 
@@ -66,11 +64,15 @@ APPLIANCE_API_RESPONSE = """
                     null
                 ]
             ], 
-            "uuid": "d7d927433b6849e4a76b6dd9d87f717f"
+            "_type": "publickey", 
+            "public_key": "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAuOg/gIR9szQ0IcPjqD1jlY9enJETyppW39MAH0WV1LqR+/ULulG4uBUS/HBwvS7ggu3P6mj4i2hH9Kz9JGwnkuhxMJu3d/b/2Z7/1hBkQls5BKTzSoYnPCVYfvPyNXzRHEcRPjyfGcrIYz2CU4g5Ei2f0IgRngamDQrTU33QLosoaJqfw0pvX2SdFyFRmJkY6vH7j66ciXl2bfUUdf1KaoadkD+n59U6EiURrholSlaZp0gECjx0dM4mZUD0DqjWGll0NmnM4NIpCl+lTOrFLicJBgPuAnsrqp8HjGEHweRoPwFkKpcPkfyV+k0o/bltu3Lyd8KLJrVzYAUXRnLRpw== dehaynes@snow.badc.rl.ac.uk", 
+            "account": "/users/bcumberbat"
         }, 
         "item_03": {
-            "_type": "directory", 
+            "description": "Raw satellite data", 
             "mount_path": "/group_workspaces/stfcloud/eumetsat/2014", 
+            "_type": "directory", 
+            "uuid": "bf43217827ab4407adaef824a060bf1f", 
             "_links": [
                 [
                     "Public key", 
@@ -81,13 +83,13 @@ APPLIANCE_API_RESPONSE = """
                     [], 
                     null
                 ]
-            ], 
-            "uuid": "0dfe416064c649028bc2fdf66e227d0e", 
-            "description": "Raw satellite data"
+            ]
         }, 
         "item_04": {
-            "_type": "directory", 
+            "description": "Modelling data workspace", 
             "mount_path": "/group_workspaces/stfcloud/moum/scratch", 
+            "_type": "directory", 
+            "uuid": "f8436e05546f4f149e2fc2842caf45c2", 
             "_links": [
                 [
                     "Public key", 
@@ -98,9 +100,24 @@ APPLIANCE_API_RESPONSE = """
                     [], 
                     null
                 ]
+            ]
+        }, 
+        "item_05": {
+            "purpose": "Headless VM for console access", 
+            "uuid": "8de53b1202354413be3d39b57bf40e1d", 
+            "template": "bastion_host", 
+            "_links": [
+                [
+                    "Image", 
+                    "canonical", 
+                    "/resource/{}", 
+                    102, 
+                    "get", 
+                    [], 
+                    null
+                ]
             ], 
-            "uuid": "9848041e37964b449cd7949d12c42cf2", 
-            "description": "Modelling data workspace"
+            "_type": "cataloguechoice"
         }
     }, 
     "options": {}
